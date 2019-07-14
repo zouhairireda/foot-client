4584e4afa5ba9fc8fe1c706974561ed871b59865
Reda Zouhairi <zouhairireda@gmail.com>
2019-07-14 21:54:49 +0200

    add router for appp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MatchListComponent } from './resources/matches/match-list/match-list.component';
+import { appRoutes } from './app.route';
 import { MatchDetailComponent } from './resources/matches/match-detail/match-detail.component';
 
 @NgModule({
@@ -16,7 +18,8 @@ import { MatchDetailComponent } from './resources/matches/match-detail/match-det
     MatchDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
