2064fe4bbffd41f8e8b3b2f3205526ca316b2e89
Reda Zouhairi <zouhairireda@gmail.com>
2019-07-14 21:45:17 +0200

    add components for match view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MatchListComponent } from './resources/matches/match-list/match-list.component';
+import { MatchDetailComponent } from './resources/matches/match-detail/match-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    MatchListComponent,
+    MatchDetailComponent
   ],
   imports: [
     BrowserModule
